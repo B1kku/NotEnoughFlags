468deab0e56af8a39451057ba23300ae8bb736f8
Bikku <bikku+dev@slmail.me>
2024-07-23 14:54:06 +0200

    Add new flag, gravity-blocks-fall

---
+++ src/main/kotlin/com/github/b1kku/notenoughflags/flags/GravityBlocksFall.kt
@@ -0,0 +1,21 @@
+package com.github.b1kku.notenoughflags.flags
+
+import com.github.b1kku.notenoughflags.utils.WorldGuardUtils
+import com.sk89q.worldguard.protection.flags.StateFlag
+import org.bukkit.block.Block
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityChangeBlockEvent
+
+class GravityBlocksFall : Listener, FlagImplementor {
+
+  override val flag = StateFlag("gravity-blocks-fall", true)
+  override val description = "Prevent gravity-affected blocks from falling"
+
+  @EventHandler(ignoreCancelled = true)
+  fun onBlockChange(event: EntityChangeBlockEvent) {
+    if (!event.block.type.hasGravity()) return
+    if (WorldGuardUtils.isFlagAllowed(event.block, this.flag)) return
+    event.isCancelled = true
+  }
+}
--- src/main/kotlin/com/github/b1kku/notenoughflags/flags/VillagerTrade.kt
+++ src/main/kotlin/com/github/b1kku/notenoughflags/flags/VillagerTrade.kt
@@ -2,12 +2,12 @@ package com.github.b1kku.notenoughflags.flags
 
 import com.github.b1kku.notenoughflags.utils.WorldGuardUtils
 import com.sk89q.worldguard.protection.flags.StateFlag
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.inventory.EquipmentSlot
-import org.bukkit.configuration.ConfigurationSection
 
 class VillagerTrade : Listener, FlagImplementor, ConfigurableFlag {
 
@@ -25,9 +25,8 @@ class VillagerTrade : Listener, FlagImplementor, ConfigurableFlag {
   }
 
   override fun updateConfig(config: ConfigurationSection?) {
-    config!!.getBoolean(wanderingTraderConfig.first).let {
-      if (!it) isValidEntity = { entityType -> entityType == EntityType.VILLAGER }
-    }
+    if (!config!!.getBoolean(wanderingTraderConfig.first))
+        isValidEntity = { entityType -> entityType == EntityType.VILLAGER }
   }
 
   @EventHandler(ignoreCancelled = true)
